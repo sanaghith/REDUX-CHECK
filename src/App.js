eb684dbee7c633dc477431d5d26ab7948a44e15e
sanaghith <ghithsana20@gmail.com>
2024-08-06 10:05:09 +0100

    second part

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import AddTodo from './AddTodo';
+import './App.css';
+import TodoList from './TodoList';
+
+
+function App() {
+
+  const [filter , setFilter] = useState("ALL")
+
+
+
+  return (
+    <div className="App">
+
+
+      <AddTodo/>
+
+      <button onClick={()=>setFilter(true)}> Done </button>
+      <button onClick={()=>setFilter(false)}> Not Done </button>
+      <button onClick={()=>setFilter("ALL")}> All </button>
+
+
+
+      <TodoList filter={filter}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import {  configureStore} from "@reduxjs/toolkit"
+import { todoReducer } from "./slices/todoSlice"
+
+
+
+
+export const store = configureStore({
+    reducer : {
+    todoReducer  
+}
+})
+export default store
